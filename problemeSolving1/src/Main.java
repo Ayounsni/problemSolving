e22934e991c6a244356fe3723cbaee978ce018a5
ayounsni <Ayoubsnini@gmail.com>
2024-09-30 12:23:30 +0100

    Done1

---
+++ problemeSolving1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Integer nombre = 1687;
+        Integer result = Calcule.somme(nombre);
+
+        System.out.println("resultat " + result);
+
+
+
+    }
+}
